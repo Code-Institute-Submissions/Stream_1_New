bd1a73badbc6ab58fc47590c1f9d2d83097ece97
Ryan Ware <ryan.ware1987@gmail.com>
2018-06-12 18:34:33 +0100

    added footer and edit map function

--- app/js/controller.js
+++ app/js/controller.js
@@ -9,40 +9,16 @@ angular.module('myApp.Controllers',[])
  $scope.title = 'Our Music'
 })
 
-//.controller('AboutCtrl', function($scope) {
-// $scope.title = 'About Us'	
-//})
+
 .controller('AboutCtrl', function($scope, uiGmapGoogleMapApi) {
  $scope.title = 'About Us'	
-
- 		$scope.map = {
-          center: {
-            latitude: 43.6722780,
-            longitude: -79.3745125
-          },
-          zoom: 10
-        };
-
-      var initializeMap = function() {
-
-         $scope.location = {
-         	latitude: 43.6722780,
-            longitude: -79.3745125
-         }; 
-        
-
-        
-      }
-
-        uiGmapGoogleMapApi.then(function(maps) {
-    initializeMap();
-});
 })
 
 .controller('scheduleCtrl', function($scope) {
  $scope.loading = false;
  $scope.title = 'Book a show with us!'	
 })
+
 
 .controller('Stream1Ctrl', ['$scope', 'firebase', function($scope, $firebase) {
 	var ref = new Firebase('https://project1-a5f4c.firebaseio.com/'); 
